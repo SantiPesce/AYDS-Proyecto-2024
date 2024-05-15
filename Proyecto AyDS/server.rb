fb9b8a0a5e1e08e913adf30ec7cf19812c24c910
JuanBortol <juanbortol@gmail.com>
2024-05-15 14:50:43 -0300

    Borrada carpeta proj(duplicaba info), integrado archivo app.rb a server.rb y se agrego el archivo login.erb y register.erb a views

--- Proyecto AyDS/server.rb
+++ Proyecto AyDS/server.rb
@@ -1,11 +1,25 @@
-require 'sinatra/base'
+require 'sinatra'
+require 'sinatra/activerecord'
+
+set :database_file, './config/database.yml'
+
+require './models/user'
 
 class App < Sinatra::Application
   def initialize(app = nil)
     super()
   end
 
-  get '/' do
-    'Welcome'
+  get '/login' do
+    erb :login
+  end
+
+  get'/register' do
+    erb :register
+  end
+
+  get '/users' do
+    @users = User.all
+    erb :'users/index'
   end
 end
